07f00c4c49560a809076c1bfb9a7a566dff46952
Scott Page <scott.page@indysoft.com>
2020-06-22 01:40:47 -0600

    Added Dashboard.vue;

---
+++ renderer/src/views/Dashboard.vue
@@ -0,0 +1,17 @@
+<template>
+  <div></div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class Dashboard extends Vue {
+
+
+}
+</script>
+
+<style>
+
+</style>
