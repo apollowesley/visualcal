14e8f93c3884972b651e249b5c2c33f966653333
Scott Page <scott.page@indysoft.com>
2020-06-23 19:29:01 -0600

    Fixed release.sh; Updated package.json to fix release.sh; Removed unused exclude in tsconfig.json;

--- release.sh
+++ release.sh
@@ -8,10 +8,12 @@ npm run build
 popd
 
 npm run rebuild
-npm run release
+npm run build
 
 cp src/nodes/package.json dist/nodes
 cp src/nodes/*.html dist/nodes
 cp -R src/nodes/examples dist/nodes
 cp -R src/nodes/icons dist/nodes
 cp -R src/nodes/locales dist/nodes
+
+npm run release
