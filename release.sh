898c29750a5528ec5842d2532eff2f6a71931c3f
Scott Page <scott.page@indysoft.com>
2020-06-23 19:26:28 -0600

    Fix release.sh

--- release.sh
+++ release.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
+pwd
+
 pushd renderer
 npm install
 npm run build
 popd
 
-cp src/nodes/package.json dist/nodes
-cp src/nodes/*.html dist/nodes
-cp -r src/nodes/examples dist/nodes
-cp -r src/nodes/icons dist/nodes
-cp -r src/nodes/locales dist/nodes
-
 npm run rebuild
 npm run release
+
+cp src/nodes/package.json dist/nodes
+cp src/nodes/*.html dist/nodes
+cp -R src/nodes/examples dist/nodes
+cp -R src/nodes/icons dist/nodes
+cp -R src/nodes/locales dist/nodes
