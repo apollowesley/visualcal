a1d1f5d94a0a0998142df9491ed4dfc1c809207c
Scott Page <scott.page@indysoft.com>
2020-06-23 19:11:58 -0600

    Added release.sh script; Update azure-pipelines.yml to use release.sh; Removed deps from nodes package.json;

---
+++ release.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+pushd renderer
+npm run install
+npm run build
+popd
+
+cp src/nodes/package.json dist/nodes
+cp src/nodes/*.html dist/nodes
+cp -r src/nodes/examples dist/nodes
+cp -r src/nodes/icons dist/nodes
+cp -r src/nodes/locales dist/nodes
+
+npm run rebuild
+npm run release
