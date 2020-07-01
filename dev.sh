04ce3bfdf1394e1d43db64946d0e1b94baef9cdf
Scott Page <scott.page@indysoft.com>
2020-07-01 11:46:33 -0600

    Update dev script before transition to git-flow;

--- dev.sh
+++ dev.sh
@@ -2,8 +2,5 @@
 
 npm run build
 
-cp src/nodes/package.json dist/nodes
-cp src/nodes/*.html dist/nodes
-cp -R src/nodes/examples dist/nodes
-cp -R src/nodes/icons dist/nodes
-cp -R src/nodes/locales dist/nodes
+npm run copynodes
+npm run copydrivers:packagejson
