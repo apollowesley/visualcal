db0795b8526919fae910ba2763db2ff4b1c0bb44
Scott Page <scott.page@indysoft.com>
2020-07-30 22:18:08 -0600

    Removed unused IpcChannels; Good idea, just not using it ATM;

--- src/main/IPC/IpcChannel.ts
+++
@@ -1,7 +0,0 @@
-import { IpcMainEvent } from 'electron';
-import { IpcRequest } from "../../common/IpcRequest";
-
-export interface IpcChannel<TReqParams> {
-  getName(): string;
-  handle(event: IpcMainEvent, request: IpcRequest<TReqParams>): void;
-}
