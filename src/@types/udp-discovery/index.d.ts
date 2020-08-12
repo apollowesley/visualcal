24cb909186c89095014abf2e4585fe13c58202b1
Scott Page <scott.page@indysoft.com>
2020-08-11 23:08:57 -0600

    Formatting

--- src/@types/udp-discovery/index.d.ts
+++ src/@types/udp-discovery/index.d.ts
@@ -1,30 +1,30 @@
-declare module 'udp-discovery'{
+declare module 'udp-discovery' {
 
-  import { TypedEmitter } from 'tiny-typed-emitter';
+    import { TypedEmitter } from 'tiny-typed-emitter';
 
-  export interface DiscoveryConstructorOptions {
-    port?: number;
-    bindAddr?: string;
-    dgramType?: string;
-  }
+    export interface DiscoveryConstructorOptions {
+        port?: number;
+        bindAddr?: string;
+        dgramType?: string;
+    }
 
-  interface DiscoveryEvents {
-    available: (name: string, data: any, reason: string) => void;
-    unavailable: (name: string, data: any, reason: string) => void;
-    MessageBus?: (eventName: string, data: any) => void;
-  }
+    interface DiscoveryEvents {
+        available: (name: string, data: any, reason: string) => void;
+        unavailable: (name: string, data: any, reason: string) => void;
+        MessageBus?: (eventName: string, data: any) => void;
+    }
 
-  interface ServiceFunction {
-    (name: string, userData: any, interval?: number, available?: boolean): void;
-  }
+    interface ServiceFunction {
+        (name: string, userData: any, interval?: number, available?: boolean): void;
+    }
 
-  export class Discovery extends TypedEmitter<DiscoveryEvents> {
-    constructor(options?: DiscoveryConstructorOptions);
-    announce: ServiceFunction;
-    pause(name: string): boolean;
-    resume(name: string, interval?: number): boolean;
-    getData(name: string): any;
-    update: ServiceFunction;
-    sendEvent(eventName: string, data: any): void;
-  }
+    export class Discovery extends TypedEmitter<DiscoveryEvents> {
+        constructor(options?: DiscoveryConstructorOptions);
+        announce: ServiceFunction;
+        pause(name: string): boolean;
+        resume(name: string, interval?: number): boolean;
+        getData(name: string): any;
+        update: ServiceFunction;
+        sendEvent(eventName: string, data: any): void;
+    }
 }
