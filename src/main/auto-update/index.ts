647551bbb098742972106b3ff382b3a163d95bd5
Scott Page <scott.page@indysoft.com>
2020-08-20 23:01:30 -0600

    No update check on dev; Move updateWindow inside update-available event handler; Bump version for prod;

--- src/main/auto-update/index.ts
+++ src/main/auto-update/index.ts
@@ -2,54 +2,64 @@ import { autoUpdater, UpdateInfo } from 'electron-updater';
 import { ProgressInfo } from 'electron-builder';
 import log from 'electron-log';
 import { BrowserWindow } from 'electron';
-import path from 'path';
+import { isDev } from '../utils/is-dev-mode';
 
-let updateWindow: BrowserWindow;
+let updateWindow: BrowserWindow | null = null;
 
 const onError = (error: Error) => {
   console.error('Auto-update error', error);
+  if (!updateWindow) return;
   updateWindow.webContents.emit('error', error);
 };
 
 const onCheckingForUpdate = () => {
   console.info('Auto-update checking for updates');
+  if (!updateWindow) return;
   updateWindow.webContents.send('checking-for-update');
 };
 
-const onUpdateAvailable = (info: UpdateInfo) => {
+const onUpdateAvailable = async (info: UpdateInfo) => {
   console.info('Update available', info);
-  updateWindow.webContents.send('update-available', info);
+  const updateAppWindow = await global.visualCal.windowManager.showUpdateAppWindow();
+  if (!updateAppWindow) return;
+  updateWindow = updateAppWindow;
+  updateWindow.on('show', () => {
+    if (!updateWindow) return;
+    updateWindow.webContents.send('update-available', info);
+    updateWindow.webContents.send('update-app-ready', 'Checking for updates...')
+  });
 };
 
 const onUpdateNotAvailable = (info: UpdateInfo) => {
   console.info('No update available', info);
+  if (!updateWindow) return;
   updateWindow.webContents.send('update-not-available', info);
 };
 
 const onDownloadProgress = (progress: ProgressInfo) => {
   console.info(`Auto-update download progress ${progress.percent}%`);
+  if (!updateWindow) return;
   updateWindow.webContents.send('download-progress', progress);
 };
 
 const onUpdateDownloaded = (info: UpdateInfo) => {
   console.info('Update downloaded', info);
+  if (!updateWindow) return;
   updateWindow.webContents.send('update-downloaded', info);
 };
 
 export default async () => {
-  const updateAppWindow = await global.visualCal.windowManager.showUpdateAppWindow();
-  if (!updateAppWindow) throw new Error('Update app window is undefined');
-  updateWindow = updateAppWindow;
-  updateWindow.on('show', () => updateWindow.webContents.send('update-app-ready', 'Checking for updates...'));
+  if (isDev()) return Promise.resolve();
   log.transports.console.level = 'debug';
   log.transports.file.level = 'debug';
   autoUpdater.logger = log;
-  // autoUpdater.setFeedURL('https://github.com/scottpageindysoft/visualcal/releases/latest');
+  autoUpdater.autoDownload = false;
+  autoUpdater.autoInstallOnAppQuit = true;
   autoUpdater.on('error', onError);
   autoUpdater.on('checking-for-update', onCheckingForUpdate);
   autoUpdater.on('update-available', onUpdateAvailable);
   autoUpdater.on('update-not-available', onUpdateNotAvailable);
   autoUpdater.on('download-progress', onDownloadProgress);
   autoUpdater.on('update-downloaded', onUpdateDownloaded);
-  await autoUpdater.checkForUpdatesAndNotify();
+  await autoUpdater.checkForUpdates();
 };
