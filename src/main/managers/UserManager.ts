710c32d4fd43d4c28f497c4e34a29408fc64363d
Scott Page <scott.page@indysoft.com>
2020-08-26 16:57:09 -0600

    UserManager uses electron-cfg, working;

--- src/main/managers/UserManager.ts
+++ src/main/managers/UserManager.ts
@@ -17,41 +17,62 @@ interface Store {
 
 export class UserManager extends TypedEmitter<Events> {
 
-  private fStore: userStore<Store>;
+  private fStore: userStore<Store> = userStore.create<Store>('users.json', UserManagerLog);
 
   constructor() {
     super();
-    this.fStore = userStore.create<Store>('users.json', UserManagerLog);
+    this.verifyActiveUserExistsAndClearIfNot();
     this.fStore.observe('activeUserEmail', () => ipcMain.sendToAll(IpcChannels.user.active.changed, this.active));
     ipcMain.on(IpcChannels.user.active.request, (event) => event.reply(IpcChannels.user.active.response, this.active));
     UserManagerLog.info('Loaded');
   }
 
-  private get users() { return this.fStore.get('users'); }
+  private get users() { return this.fStore.get('users', []); }
 
   get active() {
-    const users = this.users;
-    if (!users) return null;
-    const user = users.find(u => u.email === this.fStore.get('activeUserEmail'));
-    if (!user) return null;
-    return user;
+    const activeUser = this.users.find(u => u.email.toLocaleUpperCase() === this.fStore.get('activeUserEmail', '').toLocaleUpperCase());
+    if (!activeUser) return null;
+    return activeUser;
   }
   set active(user: User | null) {
     if (user) {
       this.fStore.set('activeUserEmail', user.email);
     } else {
-      this.fStore.set('activeUserEmail', '');
+      this.fStore.delete('activeUserEmail');
     }
     ipcMain.sendToAll(IpcChannels.user.active.changed, this.active);
   }
 
-  get all() { return this.fStore.getAll().users; }
+  private verifyActiveUserExistsAndClearIfNot() {
+    const activeUser = this.active;
+    if (!activeUser) this.fStore.delete('activeUserEmail');
+  }
 
   add(user: User) {
     let users = this.users;
     if (!users) users = [];
     users.push(user);
     this.fStore.set('users', users);
+  }
+
+  getOne(email: string) {
+    return this.users.find(u => u.email.toLocaleUpperCase() === email.toLocaleUpperCase());
+  }
+
+  exists(email: string) { return this.getOne(email) !== undefined; }
+
+  remove(email: string) {
+    if (!this.exists(email)) return;
+    const removeAt = this.users.findIndex(u => u.email.toLocaleUpperCase() === email.toLocaleUpperCase());
+    const users = this.users;
+    users.splice(removeAt, 1);
+    if (users.length <= 0) this.clear();
+    else this.fStore.set('users', users);
+  }
+
+  clear() {
+    this.fStore.delete('activeUserEmail');
+    this.fStore.delete('users');
   }
 
 }
