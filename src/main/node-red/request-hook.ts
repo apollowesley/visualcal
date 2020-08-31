243e5efe90737f7c88457901a6eebd4d645c66ff
Scott Page <scott.page@indysoft.com>
2020-08-30 19:12:12 -0600

    Test indysoft-action-start node migration to own folder and code splittig, worked;

--- src/main/node-red/request-hook.ts
+++ src/main/node-red/request-hook.ts
@@ -19,4 +19,15 @@ export default (app: Express, apiBasePath = '/red') => {
   app.use(`${apiBasePath}/renderers/window.js`, (_, res) => res.sendFile(path.join(global.visualCal.dirs.renderers.base, 'window.js')));
   app.use(`${apiBasePath}/renderers/windows/node-red.js`, (_, res) => res.sendFile(path.join(global.visualCal.dirs.renderers.windows, 'node-red.js')));
   app.use(`${apiBasePath}/vue.min.js`, (_, res) => res.sendFile(path.join(global.visualCal.dirs.base, 'node_modules', 'vue', 'dist', 'vue.min.js')));
+  app.use('/nodes/scripts/:nodeType/frontend.js', async (req, res) => {
+    const nodeType = req.params.nodeType;
+    let file = (await fsPromises.readFile(path.join(global.visualCal.dirs.base, 'dist', 'nodes', nodeType, 'frontend.js'))).toString();
+    file = file.replace('Object.defineProperty(exports, "__esModule", { value: true })', '');
+    file = file.replace('frontend.js.map', `/nodes/scripts/${nodeType}/frontend.js.map`);
+    return res.send(file);
+  });
+  app.use('/nodes/scripts/:nodeType/frontend.js.map', (req, res) => {
+    const nodeType = req.params.nodeType;
+    return res.sendFile(path.join(global.visualCal.dirs.base, 'dist', 'nodes', nodeType, 'frontend.js.map'));
+  });
 }
---
+++ src/nodes/action-start/node.ts
@@ -0,0 +1,88 @@
+import { NodeRed, NodeResetOptions } from '../../@types/logic-server';
+import { IndySoftNodeTypeNames } from '../../constants';
+import { RuntimeNode, RuntimeProperties } from '../indysoft-action-start-types';
+import { RuntimeNode as IndySoftSectionConfigurationRuntimeNode } from '../indysoft-section-configuration-types';
+import VisualCalNodeRed from '../../main/node-red';
+import RED from 'node-red';
+
+const nodeRed = RED as NodeRed;
+
+export interface NodeRedNodeUIProperties {
+  sectionConfigId?: string;
+  name?: string;
+  category: string;
+  defaults: any;
+  credentials?: any;
+  inputs?: number;
+  outputs?: number;
+  color: string;
+  paletteLabel?: string | any;
+  label?: string | any;
+  labelStyle?: string | any;
+  inputLabels?: string[] | any;
+  outputLabels?: string[] | any;
+  icon?: string;
+  align?: 'left' | 'right';
+  button?: any;
+  oneditprepare?: () => void;
+  oneditsave?: () => void;
+  oneditcancel?: () => void;
+  oneditdelete?: () => void;
+  oneditresize?: () => void;
+  onpaletteadd?: () => void;
+  onpaletteremove?: () => void;
+}
+
+function indySoftActionStartNodeConstructor(this: RuntimeNode, config: NodeRedNodeUIProperties) {
+  nodeRed.nodes.createNode(this, config as any);
+  if (config.sectionConfigId) this.section = nodeRed.nodes.getNode(config.sectionConfigId) as IndySoftSectionConfigurationRuntimeNode;
+  if (config.name) this.name = config.name;
+  this.isRunning = false;
+  const resetStatus = () => {
+    this.isRunning = false;
+    this.status({});
+  };
+  this.on('start', () => {
+    if (!this.section) {
+      this.error('Missing secton configuration');
+      return;
+    }
+    if (this.isRunning) {
+      this.error('Already running');
+      return;
+    }
+    nodeRed.settings.resetAllConnectedNodes(this);
+    global.visualCal.communicationInterfaceManager.enableAll();
+    this.status({
+      fill: 'green',
+      shape: 'dot',
+      text: 'Running'
+    });
+    this.isRunning = true;
+    this.send({
+      payload: {
+        section: this.section.shortName,
+        action: this.name
+      }
+    });
+    global.visualCal.actionManager.stateChanged(this, 'started');
+  });
+  this.on('stop', () => {
+    if (!this.section) {
+      this.error('Missing secton configuration');
+      return;
+    }
+    resetStatus();
+    global.visualCal.communicationInterfaceManager.disableAll();
+    nodeRed.settings.resetAllConnectedInstructionNodes(this);
+    global.visualCal.actionManager.stateChanged(this, 'stopped');
+  });
+  this.on('reset', (options?: NodeResetOptions) => {
+    resetStatus();
+    nodeRed.settings.resetAllConnectedNodes(this, options);
+  });
+}
+
+module.exports = (RED: NodeRed) => {
+  RED.nodes.registerType(IndySoftNodeTypeNames.ActionStart, indySoftActionStartNodeConstructor as any);
+};
