e87a27b3c239d91788fcedbdca1789165b9ca50f
Scott Page <scott.page@indysoft.com>
2020-08-31 23:06:30 -0600

    Finished nodes builder.ts, but not tested;

--- src/nodes/builder.ts
+++ src/nodes/builder.ts
@@ -1,18 +1,27 @@
 import path from 'path';
 import fs, { promises as fsPromises } from 'fs';
+import mustache from 'mustache';
 
 interface NodeInfo {
   name: string;
+  dirName: string;
   baseFrontendBuildPath: string;
   baseRuntimeBuildPath: string;
-  distBasePath: string;
-  editContentPath: string;
-  helpContentPath: string;
-  htmlScriptContentPath: string;
-  htmlTemplatePath: string;
-  htmlOutputPath: string;
-  runtimeScriptPath: string;
-  runtimeOutputPath: string;
+  htmlEditContentBuildFilePath: string;
+  htmlHelpContentBuildFilePath: string;
+  htmlScriptContentBuildFilePath: string;
+  htmlTemplateBuildPath: string;
+  runtimeScriptBuildPath: string;
+  outputBaseDirPath: string;
+  htmlOutputFilePath: string;
+  runtimeOutputFilePath: string;
+}
+
+interface NodeTemplate {
+  nodeType: string;
+  edit: string;
+  help: string;
+  script: string;
 }
 
 interface PackageJson {
@@ -21,14 +30,16 @@ interface PackageJson {
   }
 }
 
+const objectDefinesSearchString = 'Object.defineProperty(exports, "__esModule", { value: true });';
+const sourceMapUrlSearchString = 'sourceMappingURL=frontend.js.map';
+
 const baseDirPath = path.join(__dirname, '..');
-const distDirPath = path.join(baseDirPath, 'dist');
 const srcNodesDirPath = path.join(baseDirPath, 'src', 'nodes');
-const distNodesDirPath = path.join(distDirPath, 'nodes');
 const packagejsonPath = path.join(srcNodesDirPath, 'package.json');
 const buildDirPath = path.join(baseDirPath, 'nodes-build');
 const frontendBuildDirPath = path.join(buildDirPath, 'frontend');
 const runtimeBuildDirPath = path.join(buildDirPath, 'runtime');
+const outputDirPath = path.join(buildDirPath, 'dist', 'nodes');
 
 const getPackageJson = async () => {
   return JSON.parse((await fsPromises.readFile(packagejsonPath)).toString()) as PackageJson;
@@ -37,30 +48,66 @@ const getPackageJson = async () => {
 const getNodes = async () => {
   const packageJson = await getPackageJson();
   const nodes: NodeInfo[] = [];
-  for (const [_, value] of Object.entries(packageJson['node-red'].nodes)) {
+  for (const [key, value] of Object.entries(packageJson['node-red'].nodes)) {
     const nodepath = value as string;
-    const nodeName = path.dirname(nodepath);
-    const nodeDirExists = fs.existsSync(path.join(srcNodesDirPath, nodeName));
+    const nodeDirName = path.dirname(nodepath);
+    const nodeDirExists = fs.existsSync(path.join(srcNodesDirPath, nodeDirName));
     if (nodeDirExists) {
         nodes.push({
-        name: nodeName,
-        baseFrontendBuildPath: path.join(frontendBuildDirPath, nodeName),
-        baseRuntimeBuildPath: path.join(runtimeBuildDirPath, nodeName),
-        distBasePath: path.join(distNodesDirPath, nodeName),
-        editContentPath: path.join(srcNodesDirPath, nodeName, 'edit.html'),
-        helpContentPath: path.join(srcNodesDirPath, nodeName, 'help.html'),
-        htmlScriptContentPath: path.join(frontendBuildDirPath, nodeName, 'frontend.js'),
-        htmlTemplatePath: path.join(srcNodesDirPath, nodeName, 'node.html'),
-        htmlOutputPath: path.join(distNodesDirPath, nodeName, 'node.html'),
-        runtimeScriptPath: path.join(runtimeBuildDirPath, nodeName, 'runtime.js'),
-        runtimeOutputPath: path.join(distNodesDirPath, nodeName, 'node.js')
+        name: key,
+        dirName: nodeDirName,
+        baseFrontendBuildPath: path.join(frontendBuildDirPath, nodeDirName),
+        baseRuntimeBuildPath: path.join(runtimeBuildDirPath, nodeDirName),
+        outputBaseDirPath: path.join(outputDirPath, nodeDirName),
+        htmlEditContentBuildFilePath: path.join(srcNodesDirPath, nodeDirName, 'edit.html'),
+        htmlHelpContentBuildFilePath: path.join(srcNodesDirPath, nodeDirName, 'help.html'),
+        htmlScriptContentBuildFilePath: path.join(frontendBuildDirPath, nodeDirName, 'frontend.js'),
+        htmlTemplateBuildPath: path.join(srcNodesDirPath, nodeDirName, 'node.html'),
+        htmlOutputFilePath: path.join(outputDirPath, nodeDirName, 'node.html'),
+        runtimeScriptBuildPath: path.join(runtimeBuildDirPath, nodeDirName, 'runtime.js'),
+        runtimeOutputFilePath: path.join(outputDirPath, nodeDirName, 'node.js')
       });
-      console.info(`Found node, ${nodeName}`);
+      console.info(`Found node, ${nodeDirName}`);
     } else {
-      throw new Error(`Node, ${nodeName}, is missing its source directory`);
+      throw new Error(`Node, ${nodeDirName}, is missing its source directory`);
     }
   }
   return nodes;
 }
 
-getNodes();
+const build = (nodes: NodeInfo[]) => {
+  nodes.forEach(async node => {
+    const htmlEditContent = (await fsPromises.readFile(node.htmlEditContentBuildFilePath)).toString();
+    const htmlHelpContent = (await fsPromises.readFile(node.htmlHelpContentBuildFilePath)).toString();
+    let htmlScriptContent = mustache.render((await fsPromises.readFile(node.htmlScriptContentBuildFilePath)).toString(), { nodeType: node.name });
+    htmlScriptContent = htmlScriptContent.replace(sourceMapUrlSearchString, `sourceMappingURL=nodeMaps/${node.dirName}/frontend.js.map`);
+    htmlScriptContent = htmlScriptContent.replace(objectDefinesSearchString, '');
+    const mainView: NodeTemplate = {
+      nodeType: node.name,
+      edit: htmlEditContent,
+      help: htmlHelpContent,
+      script: htmlScriptContent
+    }
+    const htmlTemplate = (await fsPromises.readFile(node.htmlTemplateBuildPath)).toString();
+    const scriptTemplate = (await fsPromises.readFile(node.runtimeScriptBuildPath)).toString();
+    const nodeHtmlContent = mustache.render(htmlTemplate, mainView);
+    const nodeScriptContent = mustache.render(scriptTemplate, mainView);
+    await fsPromises.mkdir(node.outputBaseDirPath, { recursive: true });
+    await fsPromises.writeFile(node.htmlOutputFilePath, nodeHtmlContent);
+    await fsPromises.writeFile(node.runtimeOutputFilePath, nodeScriptContent);
+    await fsPromises.copyFile(path.join(srcNodesDirPath, node.dirName, 'frontend.js.map'), path.join(node.outputBaseDirPath, 'frontend.js.map'));
+    await fsPromises.copyFile(path.join(srcNodesDirPath, node.dirName, 'runtime.js.map'), path.join(node.outputBaseDirPath, 'runtime.js.map'));
+    await fsPromises.copyFile(path.join(srcNodesDirPath, 'package.json'), path.join(outputDirPath, 'package.json'));
+  });
+}
+
+const run = async () => {
+  try {
+    const nodes = await getNodes();
+    build(nodes);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+run();
