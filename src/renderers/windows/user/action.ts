98930687bc62c0e85a988c84ef5a8056e3bd2d5f
Scott Page <scott.page@indysoft.com>
2020-09-10 10:04:46 -0600

    Updated user-action.html and user/action.ts

--- src/renderers/windows/user/action.ts
+++ src/renderers/windows/user/action.ts
@@ -3,12 +3,13 @@ import { IpcChannels } from '../../../constants';
 
 const titleElement = document.getElementById('vc-title') as HTMLHeadingElement;
 
+const imageRowElement = document.getElementById('vc-image-row') as HTMLDivElement;
+const instructionRowElement = document.getElementById('vc-instruction-row') as HTMLDivElement;
+const inputRowElement = document.getElementById('vc-input-row') as HTMLDivElement;
+
+const imageElement = document.getElementById('vc-image') as HTMLImageElement;
 const instructionElement = document.getElementById('vc-instruction') as HTMLParagraphElement;
 
-const imageRowElement = document.getElementById('vc-input-row') as HTMLDivElement;
-const imageElement = document.getElementById('vc-image') as HTMLImageElement;
-
-const inputRowElement = document.getElementById('vc-input-row') as HTMLDivElement;
 const inputPrefixLabel = document.getElementById('vc-input-prefix-text') as HTMLLabelElement;
 const inputElement = document.getElementById('vc-input') as HTMLInputElement;
 const inputAppendLabel = document.getElementById('vc-input-append-text')  as HTMLLabelElement;
@@ -16,9 +17,66 @@ const inputAppendLabel = document.getElementById('vc-input-append-text')  as HTM
 const okButton = document.getElementById('vc-btn-ok') as HTMLButtonElement;
 const cancelButton = document.getElementById('vc-btn-cancel') as HTMLButtonElement;
 
-ipcRenderer.on(IpcChannels.user.input.request, (_, request: UserInputRequest) => {
+function handleImage(request: UserInputRequest) {
+  if (request.showImage && request.fileBase64Contents) {
+    imageElement.src = request.fileBase64Contents;
+    imageRowElement.classList.remove('collapse');
+  } else {
+    imageRowElement.classList.add('collapse');
+  }
+}
+
+function handleInstruction(request: UserInputRequest) {
+  if (request.text) {
+    instructionElement.textContent = request.text;
+    instructionRowElement.classList.remove('collapse');
+  } else {
+    instructionRowElement.classList.add('collapse');
+  }
+}
+
+let initialInputRequest: UserInputRequest;
+
+okButton.addEventListener('click', () => {
+  const response: UserInputResponse = {
+    action: initialInputRequest.action,
+    nodeId: initialInputRequest.nodeId,
+    section: initialInputRequest.section,
+    cancel: false
+  };
+  switch (initialInputRequest.dataType) {
+    case 'boolean':
+      response.result = !!inputElement.value;
+      break;
+    case 'float':
+      response.result = parseFloat(inputElement.value);
+      break;
+    case 'integer':
+      response.result = parseInt(inputElement.value);
+      break;
+    case 'string':
+      response.result = inputElement.value;
+      break;
+  }
+  ipcRenderer.send(IpcChannels.user.input.result, response);
+  close();
+});
+
+cancelButton.addEventListener('click', () => {
+  const response: UserInputResponse = {
+    action: initialInputRequest.action,
+    nodeId: initialInputRequest.nodeId,
+    section: initialInputRequest.section,
+    cancel: true,
+    result: false
+  };
+  ipcRenderer.send(IpcChannels.user.input.result, response);
+  close();
+});
+
+const onUserInputRequest = (request: UserInputRequest) => {
   titleElement.innerText = request.title ? request.title : 'Missing title';
-  instructionElement.innerText = request.text ? request.text : 'Missing text';
+  handleInstruction(request);
   handleImage(request);
   inputRowElement.classList.remove('collapse');
   switch (request.dataType) {
@@ -43,64 +101,6 @@ ipcRenderer.on(IpcChannels.user.input.request, (_, request: UserInputRequest) =>
     inputAppendLabel.classList.add('collapse');
   }
   initialInputRequest = request;
-});
-
-let initialInputRequest: UserInputRequest;
-
-function handleInputOkResponse() {
-  const response: UserInputResponse = {
-    action: initialInputRequest.action,
-    nodeId: initialInputRequest.nodeId,
-    section: initialInputRequest.section,
-    cancel: false
-  };
-  switch (initialInputRequest.dataType) {
-    case 'boolean':
-      response.result = !!inputElement.value;
-      break;
-    case 'float':
-      response.result = parseFloat(inputElement.value);
-      break;
-    case 'integer':
-      response.result = parseInt(inputElement.value);
-      break;
-    case 'string':
-      response.result = inputElement.value;
-      break;
-  }
-  ipcRenderer.send(IpcChannels.user.input.result, response);
 }
 
-okButton.addEventListener('click', () => {
-  if (initialInputRequest) {
-    handleInputOkResponse();
-  } else {
-    alert('Initial input request is not defined');
-  }
-  close();
-});
-
-function handleInputCancelResponse() {
-  const response: UserInputResponse = {
-    action: initialInputRequest.action,
-    nodeId: initialInputRequest.nodeId,
-    section: initialInputRequest.section,
-    cancel: true,
-    result: false
-  };
-  ipcRenderer.send(IpcChannels.user.input.result, response);
-}
-
-cancelButton.addEventListener('click', () => {
-  if (initialInputRequest) handleInputCancelResponse();
-  close();
-});
-
-function handleImage(request: UserInputRequest) {
-  if (request.showImage && request.fileBase64Contents) {
-    imageElement.src = request.fileBase64Contents;
-    imageRowElement.classList.remove('collapse');
-  } else {
-    imageRowElement.classList.add('collapse');
-  }
-}
+ipcRenderer.on(IpcChannels.user.input.request, (_, request: UserInputRequest) => onUserInputRequest(request));
