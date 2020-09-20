884c9813d25513ccf53dd39392de5a9bdf80ac2f
Scott Page <scott.page@indysoft.com>
2020-09-20 17:11:49 -0600

    Vue works in production with history mode and express;

--- src/main/servers/express/frontend-vue-request-hook.ts
+++ src/main/servers/express/frontend-vue-request-hook.ts
@@ -1,15 +1,13 @@
 import express from 'express';
 import path from 'path';
 import { promises as fsPromises } from 'fs';
+import history from 'connect-history-api-fallback';
 
 export const hook = (app: express.Express) => {
   const distRenderersVuePath = path.join(global.visualCal.dirs.renderers.base, 'vue');
   const srcRenderersVuePath = path.join(global.visualCal.dirs.base, 'src', 'renderers', 'vue');
 
-  // TODO: Reorg code so express is standalone and the rest of the app uses it, instead of having this be in node-red dir
-  app.use('/vue', express.static(path.join(global.visualCal.dirs.public, 'vue'))); // Vue.js app
-
-  app.use(`${distRenderersVuePath}/:fileName`, async (req, res) => { // Vue.js app TS maps
+  app.use(`/vue/${distRenderersVuePath}/:fileName`, async (req, res) => { // Vue.js app TS maps
     // path.join(__dirname, '..', '..', 'dist', 'renderers', 'vue'), express.static(path.join(global.visualCal.dirs.renderers.base, 'vue'))
     const fileName = req.params.fileName;
     const filePath = path.join(distRenderersVuePath, fileName);
@@ -18,9 +16,17 @@ export const hook = (app: express.Express) => {
     return res.send(fileContents);
   });
 
-  app.use(`${srcRenderersVuePath}/:fileName`, (req, res) => {
+  app.use(`/vue/${srcRenderersVuePath}/:fileName`, (req, res) => {
     const fileName = req.params.fileName;
     const filePath = path.join(srcRenderersVuePath, fileName);
     return res.send(filePath);
   });
+
+  // TODO: Reorg code so express is standalone and the rest of the app uses it, instead of having this be in node-red dir
+  app.use('/vue', history({
+    verbose: true
+  }));
+
+  app.use('/vue', express.static(path.join(global.visualCal.dirs.public, 'vue'))); // Vue.js app
+
 }
