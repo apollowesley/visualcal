8a064c3cb5727017efd4d653e4cfc35374ade399
Scott Page <scott.page@indysoft.com>
2020-09-21 09:47:12 -0600

    Remove unused device interfaces;

--- src/drivers/devices/device-interfaces.ts
+++ src/drivers/devices/device-interfaces.ts
@@ -1,27 +1,5 @@
 type deviceClass = 'generic' | 'digital-multi-meter' | 'oscilloscope' | 'multi-product-calibrator';
 
-interface DeviceCommand {
-  type: 'read' | 'write' | 'query';
-  command: string | ArrayBuffer;
-}
-
-interface DeviceReadCommand extends DeviceCommand {
-  type: 'read';
-  readTag: string;
-}
-
-class DeviceCommandSet {
-
-  private fCommands: DeviceCommand[];
-
-  constructor() {
-    this.fCommands = [];
-  }
-
-  get commands(): DeviceCommand[] { return this.fCommands; }
-
-}
-
 export interface DeviceInfo {
   manufacturer: string;
   model: string;
