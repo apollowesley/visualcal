a67e6361f2c3eb519c6c7ed9898ee552be0f3389
Scott Page <scott.page@indysoft.com>
2020-09-24 23:53:58 -0600

    Add AutoUpdate.vue; Fix dotnet-driver.sln;

---
+++ vue-frontend/src/views/AutoUpate.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-container fluid fill-height class="grey">
+    <v-row>
+      <v-col>
+        <v-row>
+          <v-col>
+            <h1 class="text-center">Update VisualCal</h1>
+          </v-col>
+        </v-row>
+        <v-row no-gutters>
+          <v-col>
+
+          </v-col>
+        </v-row>
+        <v-row no-gutters>
+          <v-col>
+
+          </v-col>
+        </v-row>
+        <v-row class="text-center" no-gutters>
+          <v-col>
+
+          </v-col>
+        </v-row>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from 'vue-property-decorator';
+
+@Component
+export default class AutoUpdateView extends Vue {
+
+}
+</script>
+
+<style>
+
+</style>
