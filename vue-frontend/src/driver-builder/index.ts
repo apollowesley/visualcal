451e791f919fc3879b4d19c0b75aba4d1f27b162
Scott Page <scott.page@indysoft.com>
2020-10-08 08:51:36 -0600

    Added parameters to SCPIRequiredCommands that needed them;

--- vue-frontend/src/driver-builder/index.ts
+++ vue-frontend/src/driver-builder/index.ts
@@ -66,11 +66,11 @@ export const SCPIRequiredCommands: Instruction[] = [
   { name: 'System Version Query', type: 'Query', command: 'SYSTem:VERSion?', responseDataType: 'String' },
   { name: 'Status Operation Event Query', type: 'Query', command: 'STATus:OPERation:EVENt?', responseDataType: 'Number' },
   { name: 'Status Operation Condition Query', type: 'Query', command: 'STATus:OPERation:CONDition?', responseDataType: 'Number' },
-  { name: 'Status Operation Enable Command', type: 'Write', command: 'STATus:OPERation:ENABle $reqArg1' },
+  { name: 'Status Operation Enable Command', type: 'Write', command: 'STATus:OPERation:ENABle', parameters: [{ type: 'number', prompt: 'Operation status value?' }] },
   { name: 'Status Operation Enable Query', type: 'Query', command: 'STATus:OPERation:ENABle?', responseDataType: 'Number' },
   { name: 'Status Questionable Event Query', type: 'Query', command: 'STATus:QUEStionable:EVENt?', responseDataType: 'Number' },
   { name: 'Status Questionable Condition Query', type: 'Query', command: 'STATus:QUEStionable:CONDition?', responseDataType: 'Number' },
-  { name: 'Status Questionable Enable Command', type: 'Write', command: 'STATus:QUEStionable:ENABle $reqArg1' },
+  { name: 'Status Questionable Enable Command', type: 'Write', command: 'STATus:QUEStionable:ENABle', parameters: [{ type: 'number', prompt: 'Questionable status value?' }] },
   { name: 'Status Questionable Enable Query', type: 'Query', command: 'STATus:QUEStionable:ENABle?', responseDataType: 'Number' },
   { name: 'Status Preset Command', type: 'Write', command: 'STATus:PRESet' }
 ];
