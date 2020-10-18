873692edcf8404ac82add1ffadf9d0967e243079
Scott Page <scott.page@indysoft.com>
2020-10-17 23:00:48 -0600

    Add dialog to handle parameter list;

---
+++ vue-frontend/src/components/driver-builder/CommandParameterListBuilderDialog.vue
@@ -0,0 +1,100 @@
+<template>
+  <v-dialog
+    :value="shouldShow"
+    max-width="50%"
+    persistent
+    eager
+  >
+    <v-container fluid class="grey" style="minHeight: 35vh">
+      <v-row>
+        <v-col class="text-center">
+          <h4>Editing list for command parameter with prompt {{ `"${commandParameter.prompt}"` }}</h4>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <v-btn
+            @click="onAddButtonClicked"
+          >
+            Add
+          </v-btn>
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col>
+          <h4>Parameter List Options</h4>
+          <div ref="tableElement" />
+        </v-col>
+      </v-row>
+      <v-row>
+        <v-col
+          offset="5"
+        >
+          <v-btn color="primary" class="ma-2" @click="onSaveClicked">
+            Save
+          </v-btn>
+          <v-btn class="ma-2" @click="$emit('cancel')">
+            Cancel
+          </v-btn>
+        </v-col>
+      </v-row>
+    </v-container>
+  </v-dialog>
+</template>
+
+<script lang="ts">
+import { CommandParameter, CommandParameterListItem } from 'visualcal-common/src/driver-builder';
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import Tabulator from 'tabulator-tables';
+
+@Component
+export default class CommandParameterListBuilderDialog extends Vue {
+
+  @Prop({ type: Boolean, required: true }) shouldShow!: boolean; // Toggle show dialog
+  @Prop({ type: Object, required: true }) commandParameter!: CommandParameter;
+
+  private fTable?: Tabulator;
+
+  private columns: Tabulator.ColumnDefinition[] = [
+    { title: 'Value', field: 'value', editable: true, editor: 'input' },
+    { title: 'Display Text', field: 'text', editable: true, editor: 'input' }
+  ]
+
+  get tableElement() { return this.$refs.tableElement as HTMLDivElement; }
+  get table() {
+    if (!this.fTable) this.fTable = this.createTable();
+    return this.fTable;
+  }
+
+  private createTable() {
+    if (this.fTable) return this.fTable;
+    const table = new Tabulator(this.tableElement, {
+      layout: 'fitColumns',
+      columns: this.columns
+    });
+    this.fTable = table;
+    return table;
+  }
+
+  mounted() {
+    this.createTable();
+  }
+
+  async onAddButtonClicked() {
+    const newOption: CommandParameterListItem = {
+      text: '',
+      value: ''
+    }
+    await this.table.addData([newOption]);
+  }
+
+  onSaveClicked() {
+    this.$emit('save', this.table.getData());
+  }
+
+}
+</script>
+
+<style>
+
+</style>
--- vue-frontend/src/components/driver-builder/CommandParametersBuilderDialog.vue
+++ vue-frontend/src/components/driver-builder/CommandParametersBuilderDialog.vue
@@ -2,9 +2,17 @@
   <v-dialog
     :value="shouldShow"
     persistent
+    eager
     max-width="75%"
   >
-    <v-container fluid class="grey">
+    <CommandParameterListBuilderDialog
+      :should-show="shouldShowCommandParameterListBuilderDialog"
+      :command-parameter="parameterForListBuilderDialog"
+      eager
+      @save="onCommandParameterListBuilderDialogSave"
+      @cancel="shouldShowCommandParameterListBuilderDialog = false"
+    />
+    <v-container fluid class="grey" style="height: 65vh">
       <v-row>
         <v-col class="text-center">
           <h4>Instruction Command Parameter Builder</h4>
@@ -43,15 +51,25 @@
 <script lang="ts">
 import { Vue, Component, Prop, Watch } from 'vue-property-decorator';
 import Tabulator from 'tabulator-tables';
-import { CustomInstruction, CommandParameter } from 'visualcal-common/src/driver-builder';
+import { CustomInstruction, CommandParameter, CommandParameterListItem } from 'visualcal-common/src/driver-builder';
+import CommandParameterListBuilderDialog from '@/components/driver-builder/CommandParameterListBuilderDialog.vue';
 
-@Component
+@Component({
+  components: {
+    CommandParameterListBuilderDialog
+  }
+})
 export default class CommandParametersBuilderDialogComponent extends Vue {
 
   @Prop({ type: Boolean, required: true }) shouldShow!: boolean; // Toggle show dialog
   @Prop({ type: Object, required: true }) instruction!: CustomInstruction;
 
   private fTable?: Tabulator;
+  shouldShowCommandParameterListBuilderDialog = false;
+  parameterForListBuilderDialog: CommandParameter = {
+    type: 'list',
+    prompt: ''
+  }
 
   @Watch('shouldShow', { immediate: true })
   async onInstructionChanged() {
@@ -97,9 +115,31 @@ export default class CommandParametersBuilderDialogComponent extends Vue {
     table.redraw(true);
   }
 
+  private formatEditParameterListCellButton(cell: Tabulator.CellComponent) {
+    const parameter = cell.getRow().getData() as CommandParameter;
+    const isListType = parameter.type === 'list';
+    if (!isListType) {
+      const div = document.createElement('div') as HTMLDivElement;
+      div.style.backgroundColor = 'silver';
+      div.style.height = '100%';
+      div.style.width = '100%';
+      return div;
+    }
+    const button = document.createElement('button');
+    button.classList.add('tabulator-cell-button');
+    button.innerText = 'Edit List';
+    button.addEventListener('click', (e) => {
+      e.preventDefault();
+      this.parameterForListBuilderDialog = parameter;
+      this.shouldShowCommandParameterListBuilderDialog = true;
+    });
+    return button;
+  }
+
   private columns: Tabulator.ColumnDefinition[] = [
     { title: '', rowHandle: true, formatter: 'handle', headerSort: false, frozen: true, width: 30, minWidth: 30, resizable: false },
     { title: 'Parameter Type*', field: 'type', editable: true, editor: 'select', editorParams: this.getParameterTypeEditorParams, cellEdited: this.updateParameter },
+    { title: '', formatter: this.formatEditParameterListCellButton, width: '100' },
     { title: 'Prompt*', field: 'prompt', editable: true, editor: 'input', validator: 'required', minWidth: 400 },
     { title: 'Text Before', field: 'beforeText', editable: true, editor: 'input' },
     { title: 'Text After', field: 'afterText', editable: true, editor: 'input' },
@@ -144,9 +184,26 @@ export default class CommandParametersBuilderDialogComponent extends Vue {
     this.$emit('save', this.instruction, this.table.getData() as CommandParameter[]);
   }
 
+  onCommandParameterListBuilderDialogSave(items: CommandParameterListItem[]) {
+    this.shouldShowCommandParameterListBuilderDialog = false;
+    this.parameterForListBuilderDialog.listItems = items;
+  }
+
 }
 </script>
 
 <style>
-
+.tabulator-cell-button {
+  -webkit-border-radius: 5px;
+  -moz-border-radius: 5px;
+  border-radius: 5px;
+  background-image: -webkit-gradient(linear, left bottom, left top, color-stop(0.16, rgb(207, 207, 207)), color-stop(0.79, rgb(252, 252, 252)));
+  background-image: -moz-linear-gradient(center bottom, rgb(207, 207, 207) 16%, rgb(252, 252, 252) 79%);
+  background-image: linear-gradient(to top, rgb(207, 207, 207) 16%, rgb(252, 252, 252) 79%); 
+  padding: 3px;
+  border: 1px solid #000;
+  color: black;
+  text-decoration: none;
+  width: 100%
+}
 </style>
